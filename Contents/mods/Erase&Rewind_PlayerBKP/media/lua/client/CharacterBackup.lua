97afd3051f3a8b525b10ded8e6f12ced28b170e6
Stefano Scalfari <vorshim92@gmail.com>
2024-09-18 23:50:02 +0200

    better structure

--- Contents/mods/Erase&Rewind_PlayerBKP/media/lua/client/CharacterBackup.lua
+++ Contents/mods/Erase&Rewind_PlayerBKP/media/lua/client/CharacterBackup.lua
@@ -3,43 +3,9 @@ if not isClient() then return end
         local characterManagement = require('character/CharacterManagement')
         local activityCalendar = require('lib/ActivityCalendar')
         local modDataManager = require('lib/ModDataManager')
-        local DataMod = {}
+        local dataMod = require('CharacterPlayer')
 
-        --- **ModData Character**
-        DataMod.Character = {
-
-            BKP_MOD_1 = "BKP_MOD_1",
-            BKP_MOD_2 = "BKP_MOD_2",
-            isDeath = "isDeath_Bkp",
-
-            -- Chiavi specifiche per il backup periodico del player
-            BKP_1 = {
-                BOOST = "characterBoost_Bkp",
-                CALORIES = "characterCalories_Bkp",
-                LIFE_TIME = "characterLifeTime_Bkp",
-                MULTIPLIER = "characterMultiplier_Bkp",
-                PERK_DETAILS = "characterPerkDetails_Bkp",
-                PROFESSION = "characterProfession_Bkp",
-                RECIPES = "characterRecipes_Bkp",
-                TRAITS = "characterTraits_Bkp",
-                WEIGHT = "characterWeight_Bkp",
-                KILLED_ZOMBIES = "characterKilledZombies_Bkp",
-                SKILL_LIMITER = "characterSkillLimiter_Bkp"
-            },
-            BKP_2 = {
-                BOOST = "characterBoost_Bkp2",
-                CALORIES = "characterCalories_Bkp2",
-                LIFE_TIME = "characterLifeTime_Bkp2",
-                MULTIPLIER = "characterMultiplier_Bkp2",
-                PERK_DETAILS = "characterPerkDetails_Bkp2",
-                PROFESSION = "characterProfession_Bkp2",
-                RECIPES = "characterRecipes_Bkp2",
-                TRAITS = "characterTraits_Bkp2",
-                WEIGHT = "characterWeight_Bkp2",
-                KILLED_ZOMBIES = "characterKilledZombies_Bkp2",
-                SKILL_LIMITER = "characterSkillLimiter_Bkp2"
-            }
-        }
+        
 
         local function onStartSaveBkp(playerIndex, player)
             -- local player = getPlayer() -- do not use because events it self give us player
@@ -48,24 +14,24 @@ if not isClient() then return end
                 time = activityCalendar.fromSecondToDate(time)
                 local lines = {}
                 table.insert(lines, time)
-                if not modDataManager.isExists(DataMod.Character.isDeath) then
+                if not modDataManager.isExists(dataMod.Character.isDeath) then
                     print("ErasePlayerBKP: Giocatore in BKP_1")
-                    if modDataManager.isExists(DataMod.Character.BKP_MOD_1) then
-                        modDataManager.remove(DataMod.Character.BKP_MOD_1)
+                    if modDataManager.isExists(dataMod.Character.BKP_MOD_1) then
+                        modDataManager.remove(dataMod.Character.BKP_MOD_1)
                         print("ErasePlayerBKP: BKP_1 pronto per la scrittura")
                     end
 
-                    characterManagement.writeBook(player, DataMod.Character.BKP_1)
-                    modDataManager.save(DataMod.Character.BKP_MOD_1, lines)
+                    characterManagement.writeBook(player, dataMod.Character.BKP_1)
+                    modDataManager.save(dataMod.Character.BKP_MOD_1, lines)
                     print("ErasePlayerBKP: BKP_1 scritto con successo. Orario: " .. time)
-                elseif modDataManager.isExists(DataMod.Character.isDeath) then
+                elseif modDataManager.isExists(dataMod.Character.isDeath) then
                     print("ErasePlayerBKP: Giocatore in BKP_2")
-                    if modDataManager.isExists(DataMod.Character.BKP_MOD_2) then
-                        modDataManager.remove(DataMod.Character.BKP_MOD_2)
+                    if modDataManager.isExists(dataMod.Character.BKP_MOD_2) then
+                        modDataManager.remove(dataMod.Character.BKP_MOD_2)
                         print("ErasePlayerBKP: BKP_2 pronto per la scrittura")
                     end
-                    characterManagement.writeBook(player, DataMod.Character.BKP_2)
-                    modDataManager.save(DataMod.Character.BKP_MOD_2, lines)
+                    characterManagement.writeBook(player, dataMod.Character.BKP_2)
+                    modDataManager.save(dataMod.Character.BKP_MOD_2, lines)
                     print("ErasePlayerBKP: BKP_2 scritto con successo. Orario: " .. time)
                 end
             else
@@ -75,14 +41,13 @@ if not isClient() then return end
 
         Events.OnCreatePlayer.Add(onStartSaveBkp)
         Events.OnCharacterDeath.Add(function()
-            if modDataManager.isExists(DataMod.Character.isDeath) then
-                modDataManager.remove(DataMod.Character.isDeath)
+            if modDataManager.isExists(dataMod.Character.isDeath) then
+                modDataManager.remove(dataMod.Character.isDeath)
             else
-            modDataManager.save(DataMod.Character.isDeath)
+            modDataManager.save(dataMod.Character.isDeath)
             end
             end)
 
-        return DataMod
     else
         print("ErasePlayerBKP: Mod Erase&Rewind_RPGbyVorshim non attiva. Nessuna azione verrà eseguita.")
     end
---
+++ Contents/mods/Erase&Rewind_PlayerBKP/media/lua/client/CharacterPlayer.lua
@@ -0,0 +1,37 @@
+--- **ModData Character**
+local DataMod = {}
+DataMod.Character = {
+
+    BKP_MOD_1 = "BKP_MOD_1",
+    BKP_MOD_2 = "BKP_MOD_2",
+    isDeath = "isDeath_Bkp",
+
+    -- Chiavi specifiche per il backup periodico del player
+    BKP_1 = {
+        BOOST = "characterBoost_Bkp",
+        CALORIES = "characterCalories_Bkp",
+        LIFE_TIME = "characterLifeTime_Bkp",
+        MULTIPLIER = "characterMultiplier_Bkp",
+        PERK_DETAILS = "characterPerkDetails_Bkp",
+        PROFESSION = "characterProfession_Bkp",
+        RECIPES = "characterRecipes_Bkp",
+        TRAITS = "characterTraits_Bkp",
+        WEIGHT = "characterWeight_Bkp",
+        KILLED_ZOMBIES = "characterKilledZombies_Bkp",
+        SKILL_LIMITER = "characterSkillLimiter_Bkp"
+    },
+    BKP_2 = {
+        BOOST = "characterBoost_Bkp2",
+        CALORIES = "characterCalories_Bkp2",
+        LIFE_TIME = "characterLifeTime_Bkp2",
+        MULTIPLIER = "characterMultiplier_Bkp2",
+        PERK_DETAILS = "characterPerkDetails_Bkp2",
+        PROFESSION = "characterProfession_Bkp2",
+        RECIPES = "characterRecipes_Bkp2",
+        TRAITS = "characterTraits_Bkp2",
+        WEIGHT = "characterWeight_Bkp2",
+        KILLED_ZOMBIES = "characterKilledZombies_Bkp2",
+        SKILL_LIMITER = "characterSkillLimiter_Bkp2"
+    }
+}
+return DataMod

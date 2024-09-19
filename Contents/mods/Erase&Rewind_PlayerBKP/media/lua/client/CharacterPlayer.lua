64ec5f5687dbbf2b78f60c19cdb081c1a88a1827
Stefano Scalfari <vorshim92@gmail.com>
2024-09-19 03:15:37 +0200

    test newdb

--- Contents/mods/Erase&Rewind_PlayerBKP/media/lua/client/CharacterPlayer.lua
+++ Contents/mods/Erase&Rewind_PlayerBKP/media/lua/client/CharacterPlayer.lua
@@ -8,41 +8,39 @@ ModData.Character = {
 
     -- Chiavi specifiche per il backup periodico del player
     BKP_1 = {
-        BOOST = "characterBoost_Bkp",
-        CALORIES = "characterCalories_Bkp",
-        LIFE_TIME = "characterLifeTime_Bkp",
-        MULTIPLIER = "characterMultiplier_Bkp",
-        PERK_DETAILS = "characterPerkDetails_Bkp",
-        PROFESSION = "characterProfession_Bkp",
-        RECIPES = "characterRecipes_Bkp",
-        TRAITS = "characterTraits_Bkp",
-        WEIGHT = "characterWeight_Bkp",
-        KILLED_ZOMBIES = "characterKilledZombies_Bkp",
+        ["BOOST"] = {},
+        ["CALORIES"] = {},
+        ["LIFE_TIME"] = {},
+        ["MULTIPLIER"] = {},
+        ["PERK_DETAILS"] = {},
+        ["PROFESSION"] = {},
+        ["RECIPES"] = {},
+        ["TRAITS"] = {},
+        ["WEIGHT"] = {},
+        ["KILLED_ZOMBIES"] = {}
     },
     BKP_2 = {
-        BOOST = "characterBoost_Bkp2",
-        CALORIES = "characterCalories_Bkp2",
-        LIFE_TIME = "characterLifeTime_Bkp2",
-        MULTIPLIER = "characterMultiplier_Bkp2",
-        PERK_DETAILS = "characterPerkDetails_Bkp2",
-        PROFESSION = "characterProfession_Bkp2",
-        RECIPES = "characterRecipes_Bkp2",
-        TRAITS = "characterTraits_Bkp2",
-        WEIGHT = "characterWeight_Bkp2",
-        KILLED_ZOMBIES = "characterKilledZombies_Bkp2",
+        ["BOOST"] = {},
+        ["CALORIES"] = {},
+        ["LIFE_TIME"] = {},
+        ["MULTIPLIER"] = {},
+        ["PERK_DETAILS"] = {},
+        ["PROFESSION"] = {},
+        ["RECIPES"] = {},
+        ["TRAITS"] = {},
+        ["WEIGHT"] = {},
+        ["KILLED_ZOMBIES"] = {}
     }
 }
 
 if getActivatedMods():contains("SkillLimiter_fix") then
-    ModData.Character.BKP_1.SKILL_LIMITER = "characterSkillLimiter_Bkp"
-    ModData.Character.BKP_2.SKILL_LIMITER = "characterSkillLimiter_Bkp2"
+    ModData.Character.BKP_1["SKILL_LIMITER"] = {}
+    ModData.Character.BKP_2["SKILL_LIMITER"] = {}
 end
 if getActivatedMods():contains("SurvivalRewards") then
-    ModData.Character.BKP_1.kilMilReached = "characterKilMilReached_Bkp"
-    ModData.Character.BKP_2.kilMilReached = "characterKilMilReached_Bkp2"
-
-    ModData.Character.BKP_1.milReached = "characterMilReached_Bkp"
-    ModData.Character.BKP_2.milReached = "characterMilReached_Bkp2"
-end
+    ModData.Character.BKP_1["kilMilReached"] = {}
+    ModData.Character.BKP_2["kilMilReached"] = {}
+    ModData.Character.BKP_1["milReached"] = {}
+    ModData.Character.BKP_2["milReached"] = {}end
 
 return ModData
